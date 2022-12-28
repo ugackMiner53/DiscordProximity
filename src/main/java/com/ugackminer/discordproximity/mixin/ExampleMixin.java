5f2631e79d5f7a2328cddff1b88d02a9db41a018
ugackMiner <ugackMiner@gmail.com>
2022-12-28 09:15:30 -0500

    Initial commit of files

---
+++ src/main/java/com/ugackminer/discordproximity/mixin/ExampleMixin.java
@@ -0,0 +1,17 @@
+package com.ugackminer.discordproximity.mixin;
+
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import com.ugackminer.discordproximity.DiscordProximity;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		DiscordProximity.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
